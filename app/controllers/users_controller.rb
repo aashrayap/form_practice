e23b1e08f920843df2655cbd31e089d7b541587a
aashraya <aashraya_p@hotmail.com>
2016-08-18 22:05:39 -0500

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+    @user=User.all
+  end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user=User.new(whitelisted)
+    if @user.save 
+      flash[:success]="congrats"
+      redirect_to users_path
+    else
+      flash[:error]="error"
+      render :new
+    end
+  end
+
+
+
+
+  private 
+  def whitelisted
+    params.require(:user).permit(:username,:email,:password)
+  end
+end
